23f3e6a4779f3d7ca61518a5a3920f7241698680
kBrent <brent.keersmaekers@gmail.com>
2015-06-30 11:10:58 +0200

    User entities zijn af

--- src/ProjectWhisky/entities/User.php
+++ src/ProjectWhisky/entities/User.php
@@ -38,9 +38,113 @@ class User
         return $user;
     }
     
+    //getters
+    
     public function getId()
     {
         return $this -> id;
     }
-
+    
+    public function getUsername()
+    {
+        return $this -> username;
+    }
+    
+    public function getPassword()
+    {
+        return $this -> password;
+    }
+    
+    public function getEmail()
+    {
+        return $this -> email;
+    }
+    
+    public function getFirstname()
+    {
+        return $this -> firstname;
+    }
+    
+    public function getLastname()
+    {
+        return $this -> Lastname;
+    }
+        
+    public function getAdmin()
+    {
+        return $this -> admin;
+    }
+        
+    public function getBlocked()
+    {
+        return $this -> blocked;
+    }
+            
+    public function getImagePath()
+    {
+        return $this -> imagePath;
+    }
+            
+    public function getRegistrationDate()
+    {
+        return $this -> registrationDate;
+    }
+    
+    //setters
+    
+    public function setId($id)
+    {
+        $this -> id = $id;
+    }
+    
+    public function setUsername($username)
+    {
+        $this -> username = $username;
+    }
+    
+    public function setPassword($password)
+    {
+        $this -> password = $password;
+    }
+    
+    public function setPassword($password)
+    {
+        $this -> password = $password;
+    }
+    
+    public function setEmail($email)
+    {
+        $this -> email = $email;
+    }
+    
+    public function setFirstname($firstname)
+    {
+        $this -> firstname = $firstname;
+    }
+    
+    public function setLastname($lastname)
+    {
+        $this -> lastname = $lastname;
+    }
+    
+    public function setAdmin($admin)
+    {
+        $this -> admin = $admin;
+    }
+    
+    public function setBlocked($blocked)
+    {
+        $this -> blocked = $blocked;
+    }
+    
+    public function setImagePath($imagePath)
+    {
+        $this -> imagePath = $imagePath;
+    }
+    
+    public function setRegistrationDate($registrationDate)
+    {
+        $this -> registrationDate = $registrationDate;
+    }
+        
 }
