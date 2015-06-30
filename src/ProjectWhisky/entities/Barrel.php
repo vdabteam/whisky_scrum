6017e4367e8b809dd28b1c29905a71625e00b820
kBrent <brent.keersmaekers@gmail.com>
2015-06-30 13:17:17 +0200

    entities whisky & barrel vervolledigd

--- src/ProjectWhisky/entities/Barrel.php
+++ src/ProjectWhisky/entities/Barrel.php
@@ -2,3 +2,39 @@
 
 namespace src\ProjectWhisky\entities;
 
+class Barrel
+{
+    private $id;
+    private $type;
+    
+    // CONSTRUCTOR
+    public function __construct($id, $type)
+    {
+        $this->id = $id;
+        $this->type = $type;
+    }
+    
+    // ACCESSORS (Getters)
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+    
+    // TRANSFORMERS (Setters)
+    
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+}
