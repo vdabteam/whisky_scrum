087ae4c570eb162e7113996db38db56187fce49c
kBrent <brent.keersmaekers@gmail.com>
2015-07-01 13:47:15 +0200

    ik neem aan dat de twig nu werkt op de productpaginas

---
+++ whiskypage_test.php
@@ -0,0 +1,20 @@
+<?php
+use src\ProjectWhisky\business\WhiskyBusiness;
+use Doctrine\Common\ClassLoader;
+
+/**
+ * Connecting doctrine autoloader
+ */
+require_once'Doctrine/Common/ClassLoader.php';
+$classLoader = new ClassLoader("src");
+$classLoader->register();
+
+
+
+$whiskyBiz = new WhiskyBusiness();
+
+$whisky = $whiskyBiz->getWhisky($_GET["id"]);
+
+print("<pre>");
+print_r($whisky);
+print("</pre>");
