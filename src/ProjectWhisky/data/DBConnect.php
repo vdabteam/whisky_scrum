a626b2040d0e43e507a6e3ebe1bc8b7499618447
Ruslan <Ruslan.Valiev@PC24302-1.ad.core.vdabantwerpen.intra>
2015-07-01 13:27:00 +0200

    DBConnect improvements

--- src/ProjectWhisky/data/DBConnect.php
+++ src/ProjectWhisky/data/DBConnect.php
@@ -35,3 +35,34 @@ class DBConnect
 }
 
 
+
+// VOOR THUISGEBRUIK
+/*
+
+class DBConnect
+{
+    private $handler;
+
+    public function startConnection()
+    {
+        try
+        {
+            $this->handler = new PDO('mysql:host=172.22.14.155;dbname=whiskyclub;charset=utf8',
+                'whisky',
+                'whisky',
+                array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+
+            $this->handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $this->handler;
+        }
+        catch (PDOException $e)
+        {
+            echo $e->getMessage();
+//            echo "Problem with DB";
+            die();
+        }
+    }
+}
+
+*/
+
