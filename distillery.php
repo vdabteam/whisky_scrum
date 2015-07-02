cd662f50a0850520e5614c00efe5f7a7944a0e03
tmvdh <TomV@nderheijden.be>
2015-07-02 14:53:10 +0200

    distillery page

--- distillery.php
+++ distillery.php
@@ -8,13 +8,20 @@ use Doctrine\Common\ClassLoader;
 require_once'Doctrine/Common/ClassLoader.php';
 $classLoader = new ClassLoader("src");
 $classLoader->register();
-
-
+/* Twig autoloader*/
+require_once("lib/Twig/Autoloader.php"); 
+Twig_Autoloader::register(); 
 
 $distilleryBiz = new DistilleryBusiness();
 
 $distillery = $distilleryBiz->getDistillery($_GET["id"]);
-
+/*
 print("<pre>");
 print_r($distillery);
 print("</pre>");
+*/
+$loader = new Twig_Loader_Filesystem("src/ProjectWhisky/presentation"); 
+$twig = new Twig_Environment($loader); 
+$view = $twig->render("distillery_page.twig", array( "distillery" => $distillery));
+
+print($view); 
