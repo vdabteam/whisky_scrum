bbd11a92f66f8f33acd55f52cbfb07873c73283b
kBrent <brent.keersmaekers@gmail.com>
2015-07-06 14:30:53 +0200

    whisky page geeft nu Region weer

--- whisky.php
+++ whisky.php
@@ -7,6 +7,7 @@ use src\ProjectWhisky\business\WhiskyBusiness;
 use src\ProjectWhisky\business\CommentBusiness;
 use src\ProjectWhisky\business\BarrelBusiness;
 use src\ProjectWhisky\business\UserBusiness;
+use src\ProjectWhisky\business\DistilleryBusiness;
 use Doctrine\Common\ClassLoader;
 
 
@@ -26,8 +27,11 @@ if ((isset($_GET['id'])) && (is_int((int)$_GET['id'])))
     $whiskyBiz = new WhiskyBusiness();
     $whisky = $whiskyBiz->getWhisky($_GET["id"]);
     
-    $BarrelBiz = new BarrelBusiness();
-    $barrel = $BarrelBiz ->showBarrel($_GET["id"]);
+    $barrelBiz = new BarrelBusiness();
+    $barrel = $barrelBiz ->showBarrel($_GET["id"]);
+    
+    $distilleryBiz = new DistilleryBusiness();
+    $distillery = $distilleryBiz ->getByWhisky($_GET["id"]);
 
     $commentBiz = new CommentBusiness();
     
@@ -41,7 +45,7 @@ if ((isset($_GET['id'])) && (is_int((int)$_GET['id'])))
 
     $loader = new Twig_Loader_Filesystem("src/ProjectWhisky/presentation");
     $twig = new Twig_Environment($loader);
-    $view = $twig->render("whisky_page.twig", array("user" => $_SESSION['user'], "whisky" => $whisky, "comments" => $commentBiz->showComments($_GET["id"]), "barrel" => $barrel['type']));
+    $view = $twig->render("whisky_page.twig", array("user" => $_SESSION['user'], "whisky" => $whisky, "comments" => $commentBiz->showComments($_GET["id"]), "barrel" => $barrel['type'], "region" => $distillery['region'] ));
 
     print($view);
 
