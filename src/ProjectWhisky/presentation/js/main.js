912244a1bab636a945ce5d75d42b499417e45647
Ruslan <molotovm@gmail.com>
2015-07-06 09:42:00 +0200

    Knop

--- src/ProjectWhisky/presentation/js/main.js
+++ src/ProjectWhisky/presentation/js/main.js
@@ -89,7 +89,9 @@ jQuery(function(){
 
 
 
-    
+    document.getElementById("uploadBtn").onchange = function () {
+        document.getElementById("uploadFile").value = this.value;
+    };
 
 
 }); //END FUNCTION
