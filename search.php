b90779555db2d2d2326313509f45a5eeea632287
Ruslan <molotovm@gmail.com>
2015-07-10 14:12:05 +0200

    Ajax search

--- search.php
+++ search.php
@@ -11,6 +11,7 @@ use src\ProjectWhisky\business\BarrelBusiness;
 use src\ProjectWhisky\business\DistilleryBusiness;
 use Doctrine\Common\ClassLoader;
 
+require_once("rolestarter.php"); // gives to user role = 0 on first visit of the website: role = 0 - guest
 
 require_once('Doctrine/Common/ClassLoader.php');
 $classLoader = new ClassLoader("src");
@@ -19,45 +20,46 @@ $classLoader->register();
 require_once("lib/Twig/Autoloader.php");
 Twig_Autoloader::register();
 
-if ((isset($_GET['strength_rangeleft'])) && (isset($_GET['strength_rangeright'])) && (isset($_GET['score_rangeleft'])) && (isset($_GET['score_rangeright'])) 
+if ((isset($_GET['strength_rangeleft'])) && (isset($_GET['strength_rangeright'])) && (isset($_GET['score_rangeleft'])) && (isset($_GET['score_rangeright']))
        && (isset($_GET['age_rangeright'])) && (isset($_GET['age_rangeright'])) && (isset($_GET['barrel_id'])) && (isset($_GET['region'])) )
 {
 
     $whiskyBiz = new WhiskyBusiness();
     $whiskyList = $whiskyBiz->getWhiskyBySearch($_GET["barrel_id"], $_GET["strength_rangeleft"], $_GET["strength_rangeright"], $_GET["score_rangeleft"], $_GET["score_rangeright"], $_GET["region"],
     $_GET["age_rangeleft"], $_GET["age_rangeright"]);
-    
+
     $BarrelBiz = new BarrelBusiness();
     $barrelList = $BarrelBiz->showAllBarrels();
-    
+
     $distilleryBiz = new DistilleryBusiness();
     $regionList = $distilleryBiz->getRegionList();
-    
-    
+
+
+
     $loader = new Twig_Loader_Filesystem("src/ProjectWhisky/presentation");
     $twig = new Twig_Environment($loader);
-    
-    $view = $twig->render("whisky_search.twig", array("user" => $_SESSION['user'], "whiskies" => $whiskyList,
+
+    $view = $twig->render("whisky_search_results.twig", array("user" => $_SESSION['user'], "whiskies" => $whiskyList,
                             "barrels" => $barrelList, "distilleries" => $regionList ));
-    
+
     print($view);
 
 }
-else 
+else
 {
 
     $whiskyBiz = new WhiskyBusiness();
     $whiskyList = $whiskyBiz->getWhiskyList();
-    
+
     $BarrelBiz = new BarrelBusiness();
     $barrelList = $BarrelBiz->showAllBarrels();
-    
+
     $distilleryBiz = new DistilleryBusiness();
     $regionList = $distilleryBiz->getRegionList();
-    
+
     $loader = new Twig_Loader_Filesystem("src/ProjectWhisky/presentation");
     $twig = new Twig_Environment($loader);
-    
+
     $view = $twig->render("whisky_search.twig", array("user" => $_SESSION['user'], "whiskies" => $whiskyList,
                             "barrels" => $barrelList, "distilleries" => $regionList ));
     print($view);
--- src/ProjectWhisky/presentation/js/search2.js
+++ src/ProjectWhisky/presentation/js/search2.js
@@ -17,7 +17,17 @@ $.get('search.php?' + $( this ).serialize());
 
 
 
+
+
+
+
 $(function(){
+
+
+
+
+
+
     $("#score_range").editRangeSlider({step: 0.5, defaultValues:{min: 0, max: 10}, bounds:{min: 0, max: 10}, wheelMode: "scroll", wheelSpeed: 10, 
                                         formatter:function(val){
                                             var decimal = val - Math.round(val);
@@ -25,6 +35,40 @@ $(function(){
                                         }
     });
     $("#strength_range").editRangeSlider({step: 1, defaultValues:{min: 1, max: 100}, bounds:{min: 1, max: 100}, wheelMode: "scroll", wheelSpeed: 10});
-    $("#age_range").editRangeSlider({step: 1, defaultValues:{min: 1, max: 70}, bounds:{min: 1, max: 70}, wheelMode: "scroll", wheelSpeed: 10}); 
-         
+    $("#age_range").editRangeSlider({step: 1, defaultValues:{min: 1, max: 70}, bounds:{min: 1, max: 70}, wheelMode: "scroll", wheelSpeed: 10});
+
+
+
+    jQuery('#search_whisky_form').change(function(e) {
+        e.preventDefault();
+        jQuery.ajax({
+            type:"GET",
+            async:false,
+            url:"search.php",
+            data: jQuery("#search_whisky_form").serialize(),
+            success: function(response){
+                //jQuery(".error_reporting_registration").addClass('result_registration').css('visibility','visible');
+                //jQuery(".result_registration").html(response);
+                console.log(jQuery("#searchResultContent").html(response));
+
+            }
+        });
+    }); // End
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
