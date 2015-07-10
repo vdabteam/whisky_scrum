e44428ed02161801a4ebfa04f00e495b6629dbbd
kBrent <brent.keersmaekers@gmail.com>
2015-07-10 14:12:18 +0200

    cp_main test

---
+++ CP_main.php
@@ -0,0 +1,35 @@
+<?php
+ob_start(); 
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+session_start();
+
+use src\ProjectWhisky\business\WhiskyBusiness;
+use src\ProjectWhisky\business\BarrelBusiness;
+use src\ProjectWhisky\business\DistilleryBusiness;
+use Doctrine\Common\ClassLoader;
+
+
+require_once('Doctrine/Common/ClassLoader.php');
+$classLoader = new ClassLoader("src");
+$classLoader->register();
+
+require_once("lib/Twig/Autoloader.php");
+Twig_Autoloader::register();
+
+$whiskyBiz = new WhiskyBusiness;
+//$whiskyList = $whiskyBiz->getWhisky();
+
+$loader = new Twig_Loader_Filesystem("src/ProjectWhisky/presentation");
+$twig = new Twig_Environment($loader);
+
+$view = $twig->render("cpanel_template.twig" );
+
+print($view);
+
+
+
+
+ob_flush();
