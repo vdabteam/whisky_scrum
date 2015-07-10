cf95b72d3e1f0f4f326c170e603802f62828f74b
RogerAntonio <roger.antonio@live.be>
2015-07-10 15:56:51 +0200

    Remove scroll feature in sliders

--- src/ProjectWhisky/presentation/js/search.js
+++ src/ProjectWhisky/presentation/js/search.js
@@ -4,14 +4,14 @@ jQuery(function(){
     /**
      * Sliders on search page
      */
-    jQuery("#score_range").editRangeSlider({step: 0.5, defaultValues:{min: 0, max: 10}, bounds:{min: 0, max: 10}, wheelMode: "scroll", wheelSpeed: 10, 
+    jQuery("#score_range").editRangeSlider({step: 0.5, defaultValues:{min: 0, max: 10}, bounds:{min: 0, max: 10}, 
                                         formatter:function(val){
                                             var decimal = val - Math.round(val);
                                             return decimal == 0 ? val.toString() + ".0" : val.toString();
                                         }
     });
-    jQuery("#strength_range").editRangeSlider({step: 1, defaultValues:{min: 1, max: 100}, bounds:{min: 1, max: 100}, wheelMode: "scroll", wheelSpeed: 10});
-    jQuery("#age_range").editRangeSlider({step: 1, defaultValues:{min: 1, max: 70}, bounds:{min: 1, max: 70}, wheelMode: "scroll", wheelSpeed: 10});
+    jQuery("#strength_range").editRangeSlider({step: 1, defaultValues:{min: 1, max: 100}, bounds:{min: 1, max: 100}});
+    jQuery("#age_range").editRangeSlider({step: 1, defaultValues:{min: 1, max: 70}, bounds:{min: 1, max: 70}});
 
 
 
